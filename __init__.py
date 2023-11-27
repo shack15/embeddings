fd3a9832b146e97ec0ae65d117f809f4111d1195
Anant <anantsnh@umich.edu>
2023-11-26 17:22:35 -0800

    Add jupyter notebook demo of package

--- __init__.py
+++
@@ -1,8 +0,0 @@
-# embeddings/__init__.py
-
-# Import necessary components
-from .database import Database
-from .collection import Collection
-
-# API Key as module level variable
-api_key = None
---
+++ embeddings/__init__.py
@@ -0,0 +1,8 @@
+# embeddings/__init__.py
+
+# API Key as module level variable
+api_key = None
+
+# Import necessary components
+from .database import Database
+from .collection import Collection
