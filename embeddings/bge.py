1a75de70a16ea37b54c842faa7f24352b9944cb8
Surya <git@sdan.io>
2023-11-28 22:59:18 -0800

    fix to smaller model

--- embeddings/bge.py
+++ embeddings/bge.py
@@ -4,8 +4,8 @@ from embeddings.embedding import Embedding
 
 class BGEEmbedding(Embedding):
     def __init__(self):
-        self.tokenizer = AutoTokenizer.from_pretrained('BAAI/bge-base-en-v1.5')
-        self.model = AutoModel.from_pretrained('BAAI/bge-base-en-v1.5')
+        self.tokenizer = AutoTokenizer.from_pretrained('BAAI/bge-small-en-v1.5')
+        self.model = AutoModel.from_pretrained('BAAI/bge-small-en-v1.5')
         self.model.eval()
 
     def emb(self, sentences):
@@ -15,3 +15,14 @@ class BGEEmbedding(Embedding):
         sentence_embeddings = model_output[0][:, 0]
         sentence_embeddings = torch.nn.functional.normalize(sentence_embeddings, p=2, dim=1)
         return sentence_embeddings
+
+
+if __name__ == '__main__':
+    from time import time
+    emb = BGEEmbedding()
+    sentences = ['This is an example sentence', 'Each sentence is converted']
+    for s in sentences:
+        start = time()
+        print('embedding {}'.format(s))
+        print(emb.emb([s]))
+        print('took {}s'.format(time() - start))
